1316eeb5fdfe39116a79478ea744cbe293e0affc
Gabriel <gabriel.guerra@hotmail.de>
2023-02-06 11:17:00 -0500

    commiting changes

--- app.py
+++ app.py
@@ -1,6 +1,86 @@
-from models import Base, session, Book, engine
-import datetime
+from models import Base, session, Brands, Product, engine
+from datetime import datetime
 import csv
 import time
 
+def menu():
+    while True:
+        print('''Please select the operation you would like to perform:
+        \rV) View Product
+        \rN) Add a new Product
+        \rA) View an analysis
+        \rB) Back up of the database
+        '''
+        )
+        user_input = input("Select any of the above options")
+        if user_input.upper() in ["V", "N", "A", "B"]:
+            return user_input
+        else:
+            input('''
+            \rPlease chose one of the above options.
+            ''')
 
+def display_product_by_id(id):
+    product = session.query(Product).filter(Product.product_id==id).first()
+
+
+def clean_price(price):
+    converted_price = None
+    try:
+        converted_price = float(price)
+    except:
+        print("Price not valid.")
+        return None
+    return int(converted_price * 100)
+
+
+def clean_date(date):
+    split_date = date.split("/")
+    day = int(split_date[1])
+    month = int(split_date[0])
+    year = int(split_date[2])
+    new_date = datetime(year, month, day)
+    return new_date.date()
+
+
+def inventory_add_csv():
+    with open('inventory.csv') as csv_file:
+        tracker = 0
+        data = csv.reader(csv_file, delimiter=',')
+        for product in data:
+            if tracker == 0:
+                tracker += 1
+                continue
+            brand = product[4].strip()
+            check_brand = session.query(Brands).filter(Brands.brand_name==brand).first()
+            dollar_price = product[1][1:]
+            new_price = clean_price(dollar_price)
+            new_date = clean_date(product[3])
+
+            product = Product(brand_id=check_brand.brand_id, product_name=product[0], product_quantity=product[2], product_price=new_price, date_updated=new_date)
+            session.add(product)
+        session.commit()
+
+
+def brands_add_csv():
+    with open ('brands.csv') as f:
+        initial = 0
+        for brand in f:
+            if initial == 0:
+                initial += 1
+                continue
+            check_brand_in_db = session.query(Brands).filter(Brands.brand_name==brand).first()
+            if check_brand_in_db == None:
+                store_brand = Brands(brand_name=brand.strip())
+                session.add(store_brand)
+            else:
+                print("Brand is already stored in Database")
+        session.commit()
+
+
+if __name__ == "__main__":
+    Base.metadata.create_all(engine)
+    #brands_add_csv()
+    #inventory_add_csv()
+    #app()
+    menu()
--- models.py
+++ models.py
@@ -1,23 +1,34 @@
-from sqlalchemy import create_engine, Column, Integer, String, Date
+from sqlalchemy import create_engine, Column, Integer, String, Date, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
 
 engine = create_engine('sqlite:///inventory.db', echo=False)
 Session = sessionmaker(bind=engine)
 session = Session()
-Base = declarative_base
+Base = declarative_base()
+
 
 class Brands(Base):
     __tablename__ = "brands"
 
     brand_id = Column(Integer, primary_key=True)
-    brand_name = Column('Brand_name')
+    brand_name = Column(String, unique=True)
 
 
 class Product(Base):
     __tablename__ = "product"
 
-    product_id = Column(Integer)
+    product_id = Column(Integer, primary_key=True)
+    brand_id = Column(Integer, ForeignKey("brands.brand_id"))
+    product_name = Column(String)
+    product_quantity = Column(Integer)
+    product_price = Column(Integer)
+    date_updated = Column(Date)
+
+
+
+
 
 #python -m venv env
 #.\env\Scripts\activate
